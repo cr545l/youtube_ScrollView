7d4c7e82da0eca6bf0a20a7ff3fd281a1fd5b08a
cr545l_xps <cr545l@gmail.com>
2018-07-11 07:44:56 +0900

    First

---
+++ Assets/ScrollViewAdd.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollViewAdd : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject _elementPrefab = null;
+
+	[SerializeField]
+	private Transform _content = null;
+
+	public void AddElement ()
+	{
+		var instance = Instantiate( _elementPrefab );
+		instance.transform.SetParent( _content );
+		
+	}
+}
